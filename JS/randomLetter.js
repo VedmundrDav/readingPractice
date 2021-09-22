c15599e856f525b622060ea8baff9b6c42cb6ed3
Daniel <iban1031@gmail.com>
2021-09-21 19:20:51 -0500

    Changed toggle design to a button that's easier to use. Also, added it so that mobile user allows touch below button line (this stops button from switch word/letter while clicking the button)

--- JS/randomLetter.js
+++ JS/randomLetter.js
@@ -6,19 +6,58 @@ function randomRgbaString (alpha) {
     return `rgba(${r},${g},${b},${a})`
   }
 
-var letters = 'ABCDEFGHIJKLMNOPQRSTUVXXYZabcdefghijklmnopqrstuvwxyz';
-var words = []; 
+var letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+var words = ["Am", "The", "White", "Blue", "Black", "Pink", "Purple","Green", "Gray", "Three", "Five"]; 
 
-document.addEventListener('keypress', function(e){
-    if(e.keyCode == 32){
-        document.getElementById('header').style.opacity= 0;
-        document.getElementById('letter').innerHTML = letters[Math.floor(Math.random() * letters.length)];
-        document.body.style.background = randomRgbaString(1);
+
+function toggleAlphabet(){
+    var btn = document.getElementById('toggle-btn');
+    btn.blur();
+    if(btn.innerHTML === 'Alphabet'){
+       btn.innerHTML = "Words";
+        // document.addEventListener('touchend', function(){
+        //     document.getElementById('header').style.opacity= 0;
+        //     document.getElementById('letter').innerHTML = words[Math.floor(Math.random() * words.length)];
+        //     document.body.style.background = randomRgbaString(1);
+        // }); 
+    }else if(btn.innerHTML === "Words"){
+        btn.innerHTML = "Alphabet";
+        // document.addEventListener('touchend', function(){
+        //     document.getElementById('header').style.opacity= 0;
+        //     document.getElementById('letter').innerHTML = letters[Math.floor(Math.random() * letters.length)];
+        //     document.body.style.background = randomRgbaString(1);
+        // }); 
+    }
+
+}
+document.addEventListener('keyup', function(e){
+    var btn = document.getElementById('toggle-btn');
+    if(e.key === ' '){
+        if(btn.innerHTML === 'Alphabet'){
+            document.getElementById('header').style.opacity= 0;
+            document.getElementById('letter').innerHTML = letters[Math.floor(Math.random() * letters.length)];
+            document.body.style.background = randomRgbaString(1);
+        }else{
+            document.getElementById('header').style.opacity= 0;
+            document.getElementById('letter').innerHTML = words[Math.floor(Math.random() * words.length)];
+            document.body.style.background = randomRgbaString(1);            
+        }
     }
 });
 
-document.addEventListener('touchend', function(){
+document.getElementById('main-content').addEventListener('touchend', function(){
+    console.log("div touched");
+    var btn = document.getElementById('toggle-btn');
+    if(btn.innerHTML === 'Alphabet'){
+        document.getElementById('header').style.opacity= 0;
+        document.getElementById('letter').innerHTML = letters[Math.floor(Math.random() * letters.length)];
+        document.body.style.background = randomRgbaString(1);
+    }else{
     document.getElementById('header').style.opacity= 0;
-    document.getElementById('letter').innerHTML = letters[Math.floor(Math.random() * letters.length)];
-    document.body.style.background = randomRgbaString(1);
+     document.getElementById('letter').innerHTML = words[Math.floor(Math.random() * words.length)];
+     document.body.style.background = randomRgbaString(1);
+    }
 });
+
+
+
